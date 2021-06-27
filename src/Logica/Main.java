a91624272c9d4811986ff1fd272a7b1cbdbaeac6
AlbertoBarrantes <71743721+AlbertoBarrantes@users.noreply.github.com>
2021-06-27 10:35:38 -0600

    Commit inicial
    Se crea la clase Main y Negocios y los paquetes Logica y Presentacion

---
+++ src/Logica/Main.java
@@ -0,0 +1,13 @@
+
+package Logica;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        
+        
+    }
+    
+}
---
+++ src/Presentacion/Negocios.java
@@ -0,0 +1,6 @@
+
+package Presentacion;
+
+public class Negocios {
+    
+}
